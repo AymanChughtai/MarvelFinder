47f17bd3e7742385ad309f6d0e96b02777dfcbc7
Ayman Chughtai <67889106+AymanChughtai@users.noreply.github.com>
2020-10-29 17:04:05 -0400

    add initial Marvel Hero Finder website files

---
+++ script.js
@@ -0,0 +1,56 @@
+const app = {};
+
+app.key = `228073f89f70a5a70413d06777bf99d5`;
+
+
+app.getHero = function (name) {
+    $.ajax({
+        url: 'http://gateway.marvel.com/v1/public/characters',
+        method: 'GET',
+        format: 'json',
+        data: {
+            apikey: '228073f89f70a5a70413d06777bf99d5',
+            hash: 'd1b5db569df5a36801967bfe65b6da39',
+            ts: 1,
+            name: name,
+        }
+    }).then((data) => {
+        console.log(data.data);
+        $('.data').empty();
+        app.displayGif(data.data)
+    })
+};
+
+
+app.displayGif = function(data){
+    data.results.forEach(gif => {
+        // console.log(gif);
+        
+        // console.log(`${gifHTML}`);
+        // console.log(`${nameHTML}`);
+        const imgHTML = `<div class="hero"><img src="${gif.thumbnail.path}.jpg"></div>`
+        const gifHTML= `<div class="desc">
+        <p class=text>Background:<br><br>${gif.description}</p></div>`
+        const nameHTML = `<p>${gif.thumbnail.path}.jpg</p>`
+        $('.data').append(imgHTML);
+        $('.data').append(gifHTML);
+        
+    });
+}
+
+ 
+app.init = function(){
+    $('form').on('submit', function(event){
+        event.preventDefault();
+        const selection = $('input').val();
+        console.log(selection)
+        app.getHero(selection);
+    })
+}
+
+
+$(function () {
+    app.init();
+ })
+   
+   
